2d4bfe8ed28c4698b50ae03f42fb531c52589b4e
ayo43 <ayok43@gmail.com>
2022-08-03 14:38:16 +0100

    Finished adding all components

--- news/views.py
+++ news/views.py
@@ -5,11 +5,22 @@ API_KEY ='5d701117be3e4d5980c66f3636e005ae'
 # Create your views here.
 
 def main(request):
-    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    print(data)
-    articles = data['articles']
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        print(data)
+        articles = data['articles']
+    else:
+
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        print(data)
+        articles = data['articles']
 
     context = {
         'articles': articles
